0341219842d93a5389a2070808bf1eac263244ed
NIKITA <kandwalnikita70@gmail.com>
2024-02-04 20:51:18 +0530

    added render

--- app.py
+++ app.py
@@ -30,4 +30,6 @@ def chat():
     return jsonify({'bot_response': bot_response})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Use 0.0.0.0 to listen on all network interfaces
+    # Use a port other than 5000 if needed (e.g., 8000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
